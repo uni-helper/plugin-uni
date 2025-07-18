dd5682965da282b2a27b6b4cadc700d080258e0d
FliPPeDround <734243792@qq.com>
2025-07-18 10:12:20 +0800

    refactor: 简化默认导出并更新类型定义

--- src/index.d.ts
+++ src/index.d.ts
@@ -2,6 +2,6 @@ import type { VitePluginUniOptions } from '@dcloudio/vite-plugin-uni'
 
 import type { Plugin } from 'vite'
 
-declare const _default: (options?: VitePluginUniOptions) => Plugin[]
+declare const _default: (rawOptions?: VitePluginUniOptions) => Plugin[]
 
 export default _default
--- src/index.js
+++ src/index.js
@@ -1,6 +1,3 @@
 import uni from '@dcloudio/vite-plugin-uni'
 
-export default (options) => {
-  // @ts-expect-error 在ESM下导出cjs模块
-  return uni.default(options)
-}
+export default uni.default ?? uni
